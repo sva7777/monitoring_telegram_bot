ed682e195150966e373224f155e45613402b5a29
vasily <sva7777@gmail.com>
2023-11-02 13:08:35 +0300

    PEP-8 formatting

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ from datetime import datetime
 # ToDo: add different monitoring tools
 # ToDo: add monitoring tool parameters
 def ping(ip_address):
-    reply = subprocess.run(['ping', '-c', '3', '-n', ip_address],
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE,
-                           encoding='utf-8')
+    reply = subprocess.run(
+        ["ping", "-c", "3", "-n", ip_address],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
     if reply.returncode == 0:
         return True
     else:
         return False
+
 
 # ToDo: neet to add support many devices prestate
 prestate = None
@@ -23,15 +26,17 @@ prestate = None
 import requests
 
 
+if __name__ == "__main__":
 
+    # ToDo: add support of many devices and chats
+    # ToDo: on fly add device(+chat) and remove it from monitoring. Need make server what performs job and clint to get and modify config
 
-if __name__=="__main__":
+    # ToDo: add interraption support
 
-# ToDo: add support of many devices and chats
-# ToDo: on fly add device(+chat) and remove it from monitoring. Need make server what performs job and clint to get and modify config
-
-    if len(sys.argv) !=4:
-        print("не корректные параметры скрипта. Правильные %Токен% %chat_id% %IP адрес%")
+    if len(sys.argv) != 4:
+        print(
+            "не корректные параметры скрипта. Правильные %Токен% %chat_id% %IP адрес%"
+        )
         sys.exit(1)
     TOKEN = sys.argv[1]
     chat_id = sys.argv[2]
@@ -44,7 +49,6 @@ if __name__=="__main__":
     url_down = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message_down}"
     url_up = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message_up}"
 
-
     while True:
         state = ping(PING)
 
@@ -55,15 +59,14 @@ if __name__=="__main__":
 
             pprint("down")
 
-            #pprint(requests.get(url_down))
+            # pprint(requests.get(url_down))
         if prestate == False and state == True:
             now = datetime.now()
             current_time = now.strftime("%H:%M:%S")
             print("Current Time =", current_time)
 
             pprint("up")
-            #pprint(requests.get(url_up))
+            # pprint(requests.get(url_up))
 
-        prestate= state
+        prestate = state
         time.sleep(2)
-
