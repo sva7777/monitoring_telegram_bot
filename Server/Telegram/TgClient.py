63953adee95bf945071410346c4b3696904aa66d
sva7777 <sva7777@gmail.com>
2024-01-13 23:55:33 +0300

    #10 getupdates from telegram. code does't work now

---
+++ Server/Telegram/TgClient.py
@@ -0,0 +1,31 @@
+import aiohttp
+from typing import Optional
+from Telegram.dcs import GetUpdatesResponse, SendMessageResponse
+
+class TgClient:
+    def __init__(self, token: str = ''):
+        self.token = token
+
+    def get_url(self, method: str):
+        return f"https://api.telegram.org/bot{self.token}/{method}"
+
+    async def get_me(self) -> dict:
+        url = self.get_url("getMe")
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                return await resp.json()
+
+    async def get_updates(self, offset: Optional[int] = None, timeout: int = 0) -> dict:
+        url = self.get_url("getUpdates")
+        params = {}
+        if offset:
+            params['offset'] = offset
+        if timeout:
+            params['timeout'] = timeout
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params) as resp:
+                return await resp.json()
+
+    async def get_updates_in_objects(self, offset: Optional[int] = None, timeout: int = 0) -> GetUpdatesResponse:
+        res_dict = await self.get_updates(offset=offset, timeout=timeout)
+        return GetUpdatesResponse.Schema().load(res_dict)
